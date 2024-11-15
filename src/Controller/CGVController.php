339d7bec123fc4b8c7c669f73e583a33eee2b56a
Yassine Sfaxi <sfaxi.mahmoudyassine@gmail.com>
2024-11-15 22:13:06 +0100

    Commit bootstrap + pages

--- src/Controller/CGVController.php
+++ src/Controller/CGVController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class CGVController extends AbstractController
 {
-    #[Route('/cgv', name: 'app_c_g_v')]
+    #[Route('/cgv', name: 'app_cgv')]
     public function index(): Response
     {
         return $this->render('cgv/index.html.twig', [
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ Encore
      * ENTRY CONFIG
      *
      * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.sass) if your JavaScript imports CSS.
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
 
@@ -50,7 +50,7 @@ Encore
     // enables and configure @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
-        config.corejs = '3.38';
+        config.corejs = '3.23';
     })
 
     // enables Sass/SCSS support

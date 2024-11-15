d4b12c201a2281f33083c534eb23e99c25cb1ada
Yassine Sfaxi <sfaxi.mahmoudyassine@gmail.com>
2024-11-15 19:56:48 +0100

    Commit 2

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,17 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.sass';
+
+//import jquery
+const $ = require('jquery');
+
+//bootstrap
+require('bootstrap');
+
+//import js file
+import "./showAndHiddePassword.js";
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ Encore
      * ENTRY CONFIG
      *
      * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     * and one CSS file (e.g. app.sass) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
 
@@ -67,7 +67,7 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
